de849ae2a8f6cb51e5da7935d246dfbdeb6e1569
Shaleen Jain <shaleen@jain.sh>
2019-12-12 19:54:51 +0530

    one: complete the second part

--- 2019/one/src/main.rs
+++ 2019/one/src/main.rs
@@ -8,10 +8,25 @@ fn main() -> std::io::Result<()> {
     let mut contents = String::new();
     reader.read_to_string(&mut contents)?;
 
+    let cal_fuel = |mass: f32| (mass / 3.0).floor() - 2.0;
+
     let total_fuel: f32 = contents
         .lines()
         .map(|line| line.trim().parse::<f32>().expect("Input should an integer"))
-        .map(|mass| (mass / 3.0).floor() - 2.0)
+        .map(cal_fuel) // calculate initial fuel required for the module
+        .map(|fuel| {
+            // calculate fuel required for the fuel itself
+            let mut total = fuel;
+            let mut new_fuel = fuel;
+            loop {
+                new_fuel = cal_fuel(new_fuel);
+                if new_fuel <= 0.0 {
+                    break;
+                }
+                total += new_fuel;
+            }
+            total
+        })
         .sum();
 
     println!("Total Fuel required is {}", total_fuel);
