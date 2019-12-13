980aeb84e8e035a0fb79b0836a22379a1d18a4ca
Shaleen Jain <shaleen@jain.sh>
2019-12-13 22:49:04 +0530

    day-2: implement the 2nd puzzle

--- 2019/day-2/src/main.rs
+++ 2019/day-2/src/main.rs
@@ -8,7 +8,7 @@ fn main() -> std::io::Result<()> {
     let mut contents = String::new();
     reader.read_to_string(&mut contents)?;
 
-    let mut state: Vec<usize> = contents
+    let init_state: Vec<usize> = contents
         .split(',')
         .enumerate()
         .map(|(index, val)| {
@@ -18,6 +18,26 @@ fn main() -> std::io::Result<()> {
         })
         .collect();
 
+    // Brute force to reach the required state
+    for noun in 0..100 {
+        for verb in 0..100 {
+            let mut state = init_state.clone();
+            state[1] = noun;
+            state[2] = verb;
+            exec_intcode(&mut state);
+
+            if state[0] == 19690720 {
+                println!("final state:\n {:?}", state);
+                println!("The found alarm code is: {}", 100 * noun + verb);
+                return Ok(());
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn exec_intcode(state: &mut Vec<usize>) {
     for index in (0..state.len()).step_by(4) {
         let opcode = state[index];
         let first = state[index + 1];
@@ -31,7 +51,4 @@ fn main() -> std::io::Result<()> {
             _ => panic!("Something went wrong!"),
         }
     }
-
-    println!("final state:\n {:?}", state);
-    Ok(())
 }
